f3a70cc361b91e79d08a6ca6485fff05de68c130
FannCyii <954713025@qq.com>
2015-11-28 14:26:23 +0800

    Initial Commit

---
+++ Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by cheng on 15/11/28.
+//  Copyright © 2015年 cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
